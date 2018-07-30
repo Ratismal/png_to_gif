5fb7ac4089cf91d57b63ee8ba2278499cd28ca05
stupid cat <kirkbenm@gmail.com>
2018-07-30 10:49:34 -0600

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+const GIFEncoder = require('gifencoder');
+const Jimp = require('jimp');
+const fs = require('fs');
+const path = require('path');
+
+async function start() {
+    let p = process.argv.slice(2).join(' ');
+    if (!p) throw new Error('No path was specified.');
+
+    p = path.resolve(p);
+
+    let files = fs.readdirSync(p);
+    files = files.filter(f => f.endsWith('.png')).map(f => path.join(p, f));
+
+    console.log('Loading first frame for dimensions...');
+    let first = await Jimp.read(files[0]);
+    let height = first.bitmap.height;
+    let width = first.bitmap.width;
+    console.log('First frame is', width + 'x' + height);
+
+    console.log('Generating a gif from all the pngs in', p, '(' + files.length + ' frames)');
+    let encoder = new GIFEncoder(width, height);
+    encoder.createReadStream().pipe(fs.createWriteStream(path.join(p, 'output.gif')));
+    encoder.start();
+    encoder.setRepeat(0);
+    encoder.setDelay(20);
+    encoder.setQuality(10);
+    let i = 1;
+
+    let base = new Jimp(width, height);
+    base.background(0x1b684dff);
+
+    for (const frame of files) {
+        process.stdout.write('\rFrame ' + i++ + '/' + files.length);
+        let temp = base.clone();
+        let f = await Jimp.read(frame);
+        temp.composite(f, 0, 0);
+        encoder.addFrame(temp.bitmap.data);
+    }
+    process.stdout.write('\n');
+    encoder.finish();
+}
+
+start().then(() => {
+    console.log('Finished!');
+}).catch(err => {
+    console.error(err);
+});
